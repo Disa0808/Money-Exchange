33e2605132b19e5d81c6e5ef76ebb1d29b28bbd9
Disa0808 <levshukdenis@mail.ru>
2018-09-13 02:13:51 +0300

    Solve 1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+  var currency = currency;
+  if(currency <= 0) return {};
+  if(currency > 10000) return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+  var mapping = {
+    50: "H",
+    25: "Q",
+    10: "D",
+    5: "N",
+    1: "P"
+  }
+  var resExchange = {};
+  for(value of Object.keys(mapping).sort((a, b) => b - a)) {
+    let s = Math.floor(currency/+value);
+    if (s > 0) { resExchange[mapping[value]] = s};console.log(resExchange);
+    currency = currency % value;
+    if(currency === 0) return resExchange;
+  }
 }
