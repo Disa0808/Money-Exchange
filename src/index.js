9d81ca4112b9bd8516359d1ab55c998542b7a9fb
Disa0808 <levshukdenis@mail.ru>
2018-09-13 02:16:20 +0300

    Solution 1

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
+  
   var currency = currency;
   if(currency <= 0) return {};
   if(currency > 10000) return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+
   var mapping = {
-    50: "H",
-    25: "Q",
-    10: "D",
-    5: "N",
-    1: "P"
-  }
+                  50: "H",
+                  25: "Q",
+                  10: "D",
+                  5: "N",
+                  1: "P"
+                }
   var resExchange = {};
+
   for(value of Object.keys(mapping).sort((a, b) => b - a)) {
     let s = Math.floor(currency/+value);
-    if (s > 0) { resExchange[mapping[value]] = s};console.log(resExchange);
+    if (s > 0) { 
+      resExchange[mapping[value]] = s
+    };
     currency = currency % value;
     if(currency === 0) return resExchange;
   }
 }
+  
